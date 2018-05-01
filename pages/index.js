11f4733c383abcfcef1acd18d7a9e8a8bbcce028
Sarath <sarath.hacks@gmail.com>
2018-05-01 18:33:52 +0530

    :fire: Initial bake :tada:

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+export default () => <div className="app"><h1>Bread N Butter</h1>
+<style jsx>{`
+
+    .app {
+        background-color: #f56;
+        color: #536353;
+    }
+`}</style>
+
+<style global jsx>{
+    
+    `
+    * { padding: 0; margin:0};
+    `
+}
+</style>
+</div>
