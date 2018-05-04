e12ef6a51502a52ac4c8029e365893563d1c5f2e
Sarath <saratonite@gmx.com>
2018-05-04 08:02:41 +0530

    :dart: ui

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,12 @@ import Layout from '../components/layout/Layout';
 export default () =>
  <Layout>
     <section className="section-home">
-        <h1>Home Page</h1>
+        <header className="main-header">
+        <div className="hero-box">
+            <h1>Bread & Butter.</h1>
+            <p>A Simple Boilerplate build with Node / Next / Express / React / SSR / Sass</p>
+        </div>
+            
+        </header>
     </section>
 </Layout>
