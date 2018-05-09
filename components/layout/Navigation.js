61cf89cbfea1059c996d6524961b7ace646d7868
Sarath <saratonite@gmx.com>
2018-05-09 11:12:30 +0530

    :zap: Upgraded next 16 , Login page , style updates

--- components/layout/Navigation.js
+++ components/layout/Navigation.js
@@ -5,6 +5,7 @@ export default () => (
         <ul>
             <li><Link href="/"><a>Home</a></Link></li>
             <li><Link href="/about"><a>About</a></Link></li>
+            <li><Link href="/login"><a>Login</a></Link></li>
         </ul>
     </nav>
 )
---
+++ pages/login.js
@@ -0,0 +1,9 @@
+import Layout from '../components/layout/Layout';
+export default () => (
+    <Layout>
+        <section>
+         <h1>Login Page</h1>
+         <p>Coming Soon</p>
+        </section>
+    </Layout>
+)
