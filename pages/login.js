076a271cb83d5263c8f93151192f48287e3de127
Sarath <sarath.hacks@gmail.com>
2018-05-19 21:39:13 +0530

    :lemon: login page design

--- pages/login.js
+++ pages/login.js
@@ -2,8 +2,21 @@ import Layout from '../components/layout/Layout';
 export default () => (
     <Layout>
         <section>
-         <h1>Login Page</h1>
-         <p>Coming Soon</p>
+        
+            <div id="login-box">
+            <h1>Login Page</h1>
+                <div>
+                    <label className="label-blk">Username / Email</label>
+                    <input type="text" className="form-control"/>
+                </div>
+                <div>
+                    <label className="label-blk">Password</label>
+                    <input type="password" className="form-control" />
+                </div>
+                <div>
+                    <button className="btn btn-lg">Login</button>
+                </div>
+            </div>
         </section>
     </Layout>
 )
