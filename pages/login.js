428710ece97ff86a1a52bf1a6f94c1163fdc3b0f
Sarath <saratonite@gmx.com>
2018-05-24 14:05:35 +0530

    :wolf: Small css changes

--- pages/login.js
+++ pages/login.js
@@ -1,20 +1,24 @@
 import Layout from '../components/layout/Layout';
+import Link from 'next/link';
 export default () => (
     <Layout>
         <section>
         
             <div id="login-box">
             <h1>Login Page</h1>
-                <div>
+                <div className="form-group">
                     <label className="label-blk">Username / Email</label>
                     <input type="text" className="form-control"/>
                 </div>
-                <div>
+                <div className="form-group">
                     <label className="label-blk">Password</label>
                     <input type="password" className="form-control" />
                 </div>
-                <div>
-                    <button className="btn btn-lg">Login</button>
+                <div className="form-group">
+                    <button className="btn btn-lg">LOGIN</button>
+                </div>
+                <div className="form-group">
+                    Or <Link href="/signup"><a>Sign up here</a></Link>
                 </div>
             </div>
         </section>
