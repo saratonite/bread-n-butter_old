7b2368c1ca59278bd9cc92cec3df49586cc71eb1
Sarath <saratonite@gmx.com>
2018-05-25 14:42:12 +0530

    :watermelon: small changes

--- pages/_error.js
+++ pages/_error.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import Link from 'next/link';
 export default class Error extends React.Component {
   static getInitialProps({ res, err }) {
     const statusCode = res ? res.statusCode : err ? err.statusCode : null;
@@ -9,10 +9,11 @@ export default class Error extends React.Component {
   render() {
     return (
       <div className="error-page">
-        <h1>{this.props.statusCode
+        <h1 className="text-error">{this.props.statusCode
           ? `An error ${this.props.statusCode} occurred on server :(`
           : ':( An error occurred on client'}
         </h1>
+        <p><Link href="/"><a> >Back to home</a></Link></p>
       </div>
     )
   }
